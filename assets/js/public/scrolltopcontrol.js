9c53b192d9a13ac33dde96b0e9fb77d250318749
yejianping <yejianping1988@gmail.com>
2015-04-26 18:01:01 -0500

    top

--- assets/js/public/scrolltopcontrol.js
+++ assets/js/public/scrolltopcontrol.js
@@ -5,7 +5,7 @@ var scrolltotop={
 		scrollduration:400, //滚动过渡时间
 		fadeduration:[500,100] //淡出淡现消失
 	},
-	controlHTML:'<img src="{{ site.url }}/images/topback.png" style="width:54px; height:54px; border:0;" />', //返回顶部按钮
+	controlHTML:'<img src="/images/topback.png" style="width:54px; height:54px; border:0;" />', //返回顶部按钮
 	controlattrs:{offsetx:30,offsety:210},//返回按钮固定位置
 	anchorkeyword:"#top",
 	state:{
